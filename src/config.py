531f6c91b4ce73ada8e1f808a28a3677382df878
Joao Guilherme <joaommguilherme@gmail.com>
2021-11-06 12:37:06 +0000

    Fixed deleting all None messages bug in questions cog. Thx José Silva

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = "ODc4Njc2OTgxNTUyNjY4Njky.YSEptQ.onrF5Yn-hfPEBTEhwl7AYOnz9pI"
+BOT_TOKEN = os.getenv(BOT_TOKEN)
 CMD_PREFIX = "!"
 TIMEZONE = "Europe/Lisbon"
 POMODORO_ITERATIONS = 4
